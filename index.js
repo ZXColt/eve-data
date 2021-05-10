17ff40224d1d2a743e668594f3e04b7c7ffa83d4
Colton <zxcolt@gmail.com>
2021-05-10 08:55:56 -0500

    push to edit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const axios = require('axios').default;
-const tableToJson = require('tabletojson').Tabletojson;
+const regions = {
+	'The Forge': 10000002,
+	Domain: 10000043,
+	Heimatar: 10000030,
+	'Sinq Laison': 10000032,
+	Metropolis: 10000042,
+};
 
 async function main() {
-	var ores = fs.readFileSync('oresv2.json', (err) => {
+	var ores = fs.readFileSync('oredata.json', (err) => {
 		if (err) throw err;
 	});
 	ores = JSON.parse(ores);
@@ -13,15 +19,30 @@ async function main() {
 	);
 
 	var orePrices = request.data.prices;
+	let orem3Prices = [];
 
 	ores.forEach((ore) => {
-		var rawPrice = orePrices[ore.types_raw[0]]['sp98'];
-		console.log(rawPrice);
-		var compressedPrice = orePrices[ore.types_compressed[0]]['sp98'];
-		var vol = ore.volume;
-		var pricem3 = (ore.batch / ore.volume) * rawPrice;
-		console.log(ore.id + pricem3);
+		//console.log(ore);
+		var rawPrice = orePrices[ore.types_raw[0]].sp98;
+
+		var compressedPricem3 = undefined;
+		if (ore.types_compressed[0] != undefined) {
+			var compressedPrice = orePrices[ore.types_compressed[0]].sp98;
+			compressedPricem3 = compressedPrice / ore.volume;
+		}
+		var pricem3 = rawPrice / ore.volume;
+
+		var prices = {
+			name: ore.id,
+			'isk/m3': pricem3.toLocaleString(),
+			'compressed isk/m3':
+				compressedPricem3 != undefined
+					? compressedPricem3.toLocaleString()
+					: '0.00',
+		};
+		orem3Prices.push(prices);
 	});
+	console.log(orem3Prices);
 	//console.log(request.data.prices);
 	//write to file
 	// var js = JSON.stringify(oresjson);
